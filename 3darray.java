74250c29809e568d584fe2b129619fcc3a8d17da
RiyaVinod <e0123043@sriher.edu.in>
2025-02-24 19:16:49 +0530

    Create 3darray.java

---
+++ 3darray.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int z = s.nextInt();
+        int[][][] arr = new int[x][y][z];
+        System.out.println("Enter the elements of the 3D array:");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                for (int k = 0; k < z; k++) {
+                    arr[i][j][k] = s.nextInt();
+                }
+            }
+        }
+        System.out.println("The elements of the 3D array are:");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                for (int k = 0; k < z; k++) {
+                    System.out.print(arr[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
